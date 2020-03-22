877a3a2f4ea9357a32308388e15bc08013143df0
Robin <rojowiec@hotmail.com>
2020-03-22 17:03:06 +0100

    added config parser and training data

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ Author: Robin
 Filename: train.py
 Date: 21.03.2020
 """
+import argparse
 import json
 import os
 
@@ -18,8 +19,13 @@ from transformers import AdamW, get_linear_schedule_with_warmup
 from data import get_dataset_for_model
 from model import copy_to_device, get_model
 
-with open("data/config/bert_cls_config.json", "r", encoding="utf8") as config_file:
-    hyperparams = json.load(config_file)
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('config_file', type=str, help='Configuration file for model and training.')
+args = parser.parse_args()
+
+# "data/config/bert_cls_config.json"
+with open(args.config_file, "r", encoding="utf8") as config_file:
+    config = json.load(config_file)
 
 # init device
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -27,27 +33,27 @@ print("Device: " + str(device))
 
 data_path = "data/preprocessed/"
 model_path = "data/models/"
-model_name = hyperparams["model"]
-epochs = hyperparams["n_epochs"]
-labels = hyperparams["labels"]
+model_name = config["model"]
+epochs = config["n_epochs"]
+labels = config["labels"]
 
 # load and batch data
-train_dataset: Dataset = get_dataset_for_model(hyperparams["model"], hyperparams["labels"], hyperparams["dataset"]["train"])
+train_dataset: Dataset = get_dataset_for_model(config["model"], config["labels"], config["dataset"]["train"])
 train_loader = torch.utils.data.DataLoader(
     train_dataset,
-    batch_size=hyperparams["batch_size"], shuffle=True, num_workers=0, pin_memory=True)
+    batch_size=config["batch_size"], shuffle=True, num_workers=0, pin_memory=True)
 
-val_dataset: Dataset  = get_dataset_for_model(hyperparams["model"], hyperparams["labels"], hyperparams["dataset"]["validation"])
+val_dataset: Dataset  = get_dataset_for_model(config["model"], config["labels"], config["dataset"]["validation"])
 val_loader = torch.utils.data.DataLoader(
     val_dataset,
-    batch_size=hyperparams["batch_size"], shuffle=True, num_workers=0, pin_memory=True)
+    batch_size=config["batch_size"], shuffle=True, num_workers=0, pin_memory=True)
 
 # init model
-model = get_model(hyperparams["model"], hyperparams["model_config"], device)
+model = get_model(config["model"], config["model_config"], device)
 model.train()
 
 # init optim
-optimizer = AdamW(model.parameters(), lr=hyperparams["learning_rate"],
+optimizer = AdamW(model.parameters(), lr=config["learning_rate"],
                   correct_bias=False)  # To reproduce BertAdam specific behavior set correct_bias=False
 scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=100,
                                             num_training_steps=1000)  # PyTorch scheduler
@@ -60,7 +66,7 @@ best_accuracy = .0
 for epoch in range(1, epochs + 1):
     epoch_loss = .0
     for index, batch_data in tqdm(enumerate(train_loader), total=int(
-            len(train_dataset) / hyperparams["batch_size"])):
+            len(train_dataset) / config["batch_size"])):
         batch_data = copy_to_device(batch_data, device=device, columns=train_dataset.get_columns())
 
         # zero gradients
@@ -74,7 +80,7 @@ for epoch in range(1, epochs + 1):
         epoch_loss += loss.detach().item()
 
         # Gradient clipping is not in AdamW anymore (so you can use amp without issue)
-        torch.nn.utils.clip_grad_norm_(model.parameters(), hyperparams["max_grad_norm"])
+        torch.nn.utils.clip_grad_norm_(model.parameters(), config["max_grad_norm"])
 
         optimizer.step()
         scheduler.step()
@@ -85,7 +91,7 @@ for epoch in range(1, epochs + 1):
     model.eval()
     val_loss = .0
     for index, batch_data in tqdm(enumerate(val_loader), total=int(
-            len(train_dataset) / hyperparams["batch_size"])):
+            len(train_dataset) / config["batch_size"])):
         batch_data = copy_to_device(batch_data, device=device, columns=val_dataset.get_columns())
 
         logits = model(**batch_data)
@@ -100,5 +106,5 @@ for epoch in range(1, epochs + 1):
         best_accuracy = accuracy
         best_val_loss = val_loss
         torch.save(model.state_dict(), os.path.join(model_path, "nli_model_basic.pt"))
-    print("Val loss {0:.04f}, Accuracy: {1:.04f}".format(val_loss, accuracy))
+    print("Epoch {0}, Val loss {1:.04f}, Accuracy: {2:.04f}".format(epoch, val_loss, accuracy))
     model.train()
